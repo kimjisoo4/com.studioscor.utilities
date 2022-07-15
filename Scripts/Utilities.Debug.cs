2918f3b0fb2af7e951e729d231a5ada4ac5fa28e
kimjisoo4 <58765606+kimjisoo4@users.noreply.github.com>
2022-07-15 13:37:42 +0900

    Upload

---
+++ Scripts/Utilities.Debug.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace KimScor.Utilities
+{
+    public static partial class Utilities
+    {
+        [Conditional("UNITY_EDITOR")]
+        public static void Log(object message, Object context = null)
+        {
+            UnityEngine.Debug.Log(message, context);
+        }
+    }
+
+}
