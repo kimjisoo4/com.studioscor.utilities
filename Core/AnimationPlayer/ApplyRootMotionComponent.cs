e87e20e988f6c91ddbdf9482907ba5555c024bde
KimScor <58765606+kimjisoo4@users.noreply.github.com>
2024-01-22 07:47:50 +0900

    Changed Variable Naming
    [ Changed ]
     일부 변수의 이름을 변경함.

--- Core/AnimationPlayer/ApplyRootMotionComponent.cs
+++ Core/AnimationPlayer/ApplyRootMotionComponent.cs
@@ -18,46 +18,46 @@ namespace StudioScor.Utilities
     public class ApplyRootMotionComponent : BaseMonoBehaviour, IApplyRootMotion
     {
         [Header(" [ Apply Root Motion ] ")]
-        [SerializeField] private Animator _animator;
+        [SerializeField] private Animator _Animator;
 
-        public Animator Animator => _animator;
-        public bool ApplyRootMotion => _animator.applyRootMotion;
-        public Vector3 DeltaPosition => _deltaPosition;
-        public Quaternion DeltaRotation => _deltaRotation;
+        public Animator Animator => _Animator;
+        public bool ApplyRootMotion => _Animator.applyRootMotion;
+        public Vector3 DeltaPosition => _DeltaPosition;
+        public Quaternion DeltaRotation => _DeltaRotation;
 
         [Header(" Unity Event ")]
-        [SerializeField] private bool useUnityEvent = false;
-        [SerializeField][SCondition(nameof(useUnityEvent))] private UnityEvent<bool> _onChangedRootMotionState;
+        [SerializeField] private bool _UseUnityEvent = false;
+        [SerializeField][SCondition(nameof(_UseUnityEvent))] private UnityEvent<bool> _OnChangedRootMotionState;
 
         public event IApplyRootMotion.RootMotionStateEventHandler OnChangedRootMotionState;
 
-        protected Vector3 _deltaPosition;
-        protected Quaternion _deltaRotation;
+        protected Vector3 _DeltaPosition;
+        protected Quaternion _DeltaRotation;
 
 
         protected virtual void Reset()
         {
 #if UNITY_EDITOR
-            _animator = GetComponent<Animator>();
+            _Animator = GetComponent<Animator>();
 #endif
         }
         protected virtual void Awake()
         {
-            if (!_animator)
-                _animator = GetComponent<Animator>();
+            if (!_Animator)
+                _Animator = GetComponent<Animator>();
         }
 
         public void SetUseRootMotion(bool useRootMotion)
         {
-            if (_animator.applyRootMotion == useRootMotion)
+            if (_Animator.applyRootMotion == useRootMotion)
                 return;
 
-            _animator.applyRootMotion = useRootMotion;
+            _Animator.applyRootMotion = useRootMotion;
 
             if(!useRootMotion)
             {
-                _deltaPosition = Vector3.zero;
-                _deltaRotation = Quaternion.identity;
+                _DeltaPosition = Vector3.zero;
+                _DeltaRotation = Quaternion.identity;
             }
 
             Invoke_OnChangedRootMotionState();
@@ -68,8 +68,8 @@ namespace StudioScor.Utilities
             if (!ApplyRootMotion)
                 return;
 
-            _deltaPosition = _animator.deltaPosition;
-            _deltaRotation = _animator.deltaRotation;
+            _DeltaPosition = _Animator.deltaPosition;
+            _DeltaRotation = _Animator.deltaRotation;
 
             UpdateRootMotion();
         }
@@ -80,9 +80,9 @@ namespace StudioScor.Utilities
         {
             Log($"On Changed RootMotion State : {Animator.applyRootMotion}", Animator.applyRootMotion ? SUtility.NAME_COLOR_GREEN : SUtility.NAME_COLOR_GRAY);
 
-            if(useUnityEvent)
+            if(_UseUnityEvent)
             {
-                _onChangedRootMotionState?.Invoke(Animator.applyRootMotion);
+                _OnChangedRootMotionState?.Invoke(Animator.applyRootMotion);
             }
 
             OnChangedRootMotionState?.Invoke(this);
